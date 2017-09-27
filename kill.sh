46781834e40d187cc2f80a178b1adae20a238518
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-09-26 22:58:23 -0300

    Projeto implantado no Docker

---
+++ kill.sh
@@ -0,0 +1,6 @@
+sudo docker stop app
+sudo docker rm app
+sudo docker rmi sessionbean/app
+sudo docker stop banco
+sudo docker rm banco
+sudo docker rmi sessionbean/banco
---
+++ run.sh
@@ -0,0 +1,8 @@
+cd Postgres
+sh run.sh
+cd ../
+sudo docker stop app
+sudo docker rm app
+sudo docker rmi sessionbean/app
+sudo docker build -t sessionbean/app .
+sudo docker run -p 8080:8080 -d --name app --link banco:host-banco sessionbean/app
